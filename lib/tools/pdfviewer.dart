8dfa2ab08929912d0124aef65fe0a1a395c07c2a
Rahul Gopinath <gorahul@umich.edu>
2022-12-27 14:35:13 -0600

    PDF Viewer added

--- lib/tools/pdfviewer.dart
+++ lib/tools/pdfviewer.dart
@@ -1,5 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:kybele_gen2/nav/header.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class Document{
+  String? doc_title;
+  String? doc_path;
+  int? page_num;
+  Document(this.doc_title,this.doc_path,this.page_num);
+  static List<Document> doc_List = [
+    Document(
+      "Hello", 
+      "assets/NRPQuickEquipmentChecklist.pdf",
+      69),
+    Document(
+      "Hello", 
+      "assets/NRPQuickEquipmentChecklist.pdf",
+      69),
+    Document(
+      "Hello", 
+      "assets/NRPQuickEquipmentChecklist.pdf",
+      69),
+    Document(
+      "Hello", 
+      "assets/NRPQuickEquipmentChecklist.pdf",
+      69),
+  ];
+}
+
+class RenderScreen extends StatefulWidget {
+  RenderScreen(this.doc, {Key ? key}) : super(key: key);
+  Document doc;
+  @override
+  State<RenderScreen> createState() => _RenderScreenState();
+}
+class _RenderScreenState extends State<RenderScreen> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text(widget.doc.doc_title!)
+      ),
+      body: Container(child: SfPdfViewer.asset(
+      widget.doc.doc_path!))
+    );
+  }
+}
 
 class PDFViewer extends StatelessWidget {
   const PDFViewer({super.key});
@@ -18,9 +65,29 @@ class PDFViewer extends StatelessWidget {
                 size: 30,
               ),
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 14, vertical: 26),
+              child:
             Expanded(
-              // child: SingleChildScrollView()
-              child: Center(child: Text("PDF Viewer code here"))
+              child: SingleChildScrollView(child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:[
+                  Text("Additional Resources", style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700)),
+                  Column(children: Document.doc_List.map((doc) => ListTile(
+                    onTap:() { Navigator.push(context, MaterialPageRoute(builder: (context) => RenderScreen(doc))); },
+                    title: Text(doc.doc_title!, overflow: TextOverflow.ellipsis),
+                    subtitle: Text("${doc.page_num!} Pages"),
+                    leading: Icon(
+                      Icons.picture_as_pdf,
+                      color: Colors.red,
+                      size: 32,
+                    )
+                  )).toList()
+                  )
+                ]
+              ))
+            )
             )
           ],
         ),
