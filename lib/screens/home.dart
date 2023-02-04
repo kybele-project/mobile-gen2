594b94b086c3859b23d09f3748ab7f59491045a5
crosscountryboy123 <49216284+crosscountryboy123@users.noreply.github.com>
2023-02-03 20:44:23 -0500

    merged first page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -21,10 +21,10 @@ class HomePage extends StatelessWidget {
             SliverList(
               delegate: SliverChildListDelegate(
                 [
-                  /*
+
                   Container(
                     width: MediaQuery.of(context).size.width * 0.9,
-                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 20),
+                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
                     alignment: Alignment.center,
                     child: Image.asset(
                       'assets/kybele_purple.png',
@@ -32,7 +32,7 @@ class HomePage extends StatelessWidget {
                       fit: BoxFit.fitHeight,
                     ),
                   ),
-                   */
+
                   SizedBox(height: 20),
                   Container(
                     width: MediaQuery.of(context).size.width * 0.9,
@@ -40,7 +40,7 @@ class HomePage extends StatelessWidget {
                     alignment: Alignment.centerLeft,
                     child: Text(
                       "Tools",
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal),
                     ),
                   ),
                 ],
@@ -67,16 +67,23 @@ class HomePage extends StatelessWidget {
                   TargetOxygenSaturation(),
                 ),
                 KybeleColorfulTile(
-                  Color(0xffE2EEF9),
-                  Color(0xff436B8F),
-                  Icons.bubble_chart_rounded,
+                  Color(0xffffe9cc),
+                  Color(0xff89683e),
+                  Icons.account_tree_rounded,
+                  'NRP Modules',
+                  VideosPages(),
+                ),
+                KybeleColorfulTile(
+                  Color(0xffdaf7d9),
+                  Color(0xff458e43),
+                  Icons.feed_outlined,
                   'Forms',
                   FormsPages(),
                 ),
                 KybeleColorfulTile(
-                  Color(0xffE2EEF9),
-                  Color(0xff436B8F),
-                  Icons.bubble_chart_rounded,
+                  Color(0xfff9e1f5),
+                  Color(0xff8e4383),
+                  Icons.ondemand_video_rounded,
                   'Videos',
                   VideosPages(),
                 ),
@@ -96,98 +103,7 @@ class HomePage extends StatelessWidget {
                             ), */
               ],
             ),
-            SliverList(
-              delegate: SliverChildListDelegate([
-                SizedBox(height: 20),
-                Text(
-                  "Training Videos",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
-                ),
-                SizedBox(height: 10),
-              ]),
-            ),
 
-            SliverToBoxAdapter(
-              child: Container(
-                height: 180,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap:() {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => TutorialPage(
-                                Video.video_List[index],
-                                index,
-                              )),
-                        );
-                      }, child:Container(
-                      width: 210.0,
-                      margin: EdgeInsets.only(right:40),
-                      child: Column(
-                          children: [
-                            Image.asset(Video.video_List[index].thumbnail_path!),
-                            Container(width: 225, child:Text(Video.video_List[index].video_title!, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w100), textAlign: TextAlign.left,)),
-                            Container(width: 225, child:Text("${Video.video_List[index].video_length} mins", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100), textAlign: TextAlign.left,)),
-                          ]
-                      ),
-                    ),
-                    );
-                  },
-                ),
-              ),
-            ),
-
-            SliverList(
-              delegate: SliverChildListDelegate([
-                SizedBox(height: 20),
-                Text(
-                  "Additional Resources",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
-                ),
-                SizedBox(height: 10),
-              ]),
-            ),
-            SliverGrid.count(
-                crossAxisCount: 1,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20,
-                childAspectRatio: 5,
-                children: [
-                  KybelePDFTile(
-                    "NRP Checklist",
-                    RenderScreen(
-                      "NRP Checklist",
-                      "assets/NRPQuickEquipmentChecklist.pdf",
-                    ),
-                  ),
-                  KybelePDFTile(
-                    "A4 NRP Checklist",
-                    RenderScreen(
-                      "A4 NRP Checklist",
-                      "assets/A4 - NRP Checklist 27Oct2022.pdf",
-                    ),
-                  ),
-                  KybelePDFTile(
-                    "T-Piece Resuscitator for Lamination",
-                    RenderScreen(
-                      "T-Piece Resuscitator for Lamination",
-                      "assets/T-Piece resuscitator for lamination.pdf",
-                    ),
-                  ),
-                  KybelePDFTile(
-                    "Warmilu Thermal Gel Instructions",
-                    RenderScreen(
-                      "A4 NRP Checklist",
-                      "assets/Warmilu_thermal gel autoclave instructions.pdf",
-                    ),
-                  ),
-
-                ]
-            ),
           ],
         ),
       ),
@@ -216,28 +132,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return KybelePage.fixedNoHeader(
-        hasBottomActionButton: false,
-        bodyWidget: body(context),
+      hasBottomActionButton: false,
+      bodyWidget: body(context),
     );
   }
 }
-
-/*
-@override
-  Widget build(BuildContext context) {
-    return KybelePage.fixedNoHeader(
-        true,
-        true,
-        true,
-        true,
-        false,
-        body(context),
-        headerText: "Home",
-        headerIcon: Icons.home_rounded,
-        headerIconBkgColor: Color(0xffdddddd),
-        headerIconColor: Color(0xff555555),
-        bottomButtonText: "Log event",
-        bottomButtonMenuWidget: buttonMenu(context),
-    );
-  }
- */
