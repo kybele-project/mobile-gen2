9608f1a2dcc45071399e29e8815181a45a2cee65
crosscountryboy123 <49216284+crosscountryboy123@users.noreply.github.com>
2023-02-07 17:37:41 -0500

    oxygen saturation

--- lib/screens/oxygen_saturation.dart
+++ lib/screens/oxygen_saturation.dart
@@ -2,31 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:kybele_gen2/templates/page/page.dart';
 import 'package:provider/provider.dart';
 
-class OxygenSaturationProvider with ChangeNotifier {
+class OxygenSaturation extends StatefulWidget {
+  const OxygenSaturation({Key? key}) : super(key: key);
 
-  final List<int> _minuteIntervalList = [1000, 2000, 3000, 4000, 5000, 10000];
+  @override
+  State<OxygenSaturation> createState() => _OxygenSaturationState();
+}
+
+class _OxygenSaturationState extends State<OxygenSaturation> {
+
+  final List<String> _minuteIntervalList = ['1', '2', '3', '4', '5', '10'];
+  final List<int> _minuteMSList = [1000, 2000, 3000, 4000, 5000, 10000];
   final List<int> _lowerBound = [60, 65, 70, 75, 80, 85];
   final List<int> _upperBound = [65, 70, 75, 80, 85, 95];
 
-  late int _saturationPercentage = 75;
-  late int _minuteInterval = 0;
-  late int _status = 1;
-
-  int get saturationPercentage => _saturationPercentage;
-  int get minuteInterval => _minuteInterval;
-  int get status => _status;
-
-  void setSaturationPercentage(int nextSaturationPercentage) {
-    _saturationPercentage = nextSaturationPercentage;
-  }
-
-  void setMinuteInterval(int nextMinuteInterval) {
-    _minuteInterval = nextMinuteInterval;
-  }
-
-  void setStatus(int nextStatus) {
-    _status = nextStatus;
-  }
+  int _saturationPercentage = 60;
+  int _minuteInterval = 0;
+  int _status = 1;
 
   void updateStatus(int milliseconds) {
     int currMinute = (milliseconds/1000).floor();
@@ -51,52 +43,39 @@ class OxygenSaturationProvider with ChangeNotifier {
       currIndex = 0;
     }
     else {
-      setStatus(2);
+      setState(() {
+        _status = 2;
+      });
+
       return;
     }
 
     if (_saturationPercentage >= _lowerBound[currIndex] &&
         _saturationPercentage <= _upperBound[currIndex]) {
-      setStatus(1);
+      setState(() {
+        _status = 1;
+      });
     }
     else {
-      setStatus(0);
+      setState(() {
+        _status = 0;
+      });
     }
-  }
-}
-
-class OxygenSaturation extends StatefulWidget {
-  const OxygenSaturation({Key? key}) : super(key: key);
-
-  @override
-  State<OxygenSaturation> createState() => _OxygenSaturationState();
-}
-
-class _OxygenSaturationState extends State<OxygenSaturation> {
-
-  late double _currSat;
-
-  @override
-  void initState() {
-    _currSat = 75;
   }
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (context) => OxygenSaturationProvider(),
-        child: KybelePage.fixedWithHeader(
-            hasHeaderIcon: true,
-            headerIconColor: Colors.black,
-            headerIconBkgColor: Colors.amberAccent,
-            headerIcon: Icons.bakery_dining,
-            hasHeaderClose: true,
-            hasBottomActionButton: true,
-            bottomButtonText: "Log event",
-            bottomButtonMenuWidget: const Placeholder(),
-            headerText: "Oxygen Saturation",
-            bodyWidget: body(context),
-        ),
+    return KybelePage.fixedWithHeader(
+      hasHeaderIcon: true,
+      headerIconBkgColor: const Color(0xffE2EEF9),
+      headerIconColor: const Color(0xff436B8F),
+      headerIcon: Icons.bubble_chart_rounded,
+      hasHeaderClose: true,
+      hasBottomActionButton: true,
+      bottomButtonText: "Log event",
+      bottomButtonMenuWidget: const Placeholder(),
+      headerText: "Oxygen Saturation",
+      bodyWidget: body(context),
     );
   }
 
@@ -105,27 +84,266 @@ class _OxygenSaturationState extends State<OxygenSaturation> {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            Consumer<OxygenSaturationProvider>(
-              builder: (context, provider, widget) {
-                return Text(provider._saturationPercentage.toString());
-              }
+            Container(
+              padding: const EdgeInsets.all(20),
+              color: (_status == 2) ? Colors.white : ((_status == 1) ? Colors.green.shade50 : Colors.red.shade50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        _saturationPercentage.toString() + "%",
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      (_status == 2) ? Container() :
+                      ((_status == 1) ? Text(
+                        "SAFE",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ) : Text(
+                        "UNSAFE",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                    ],
+                  ),
+                  (_status != 2) ? Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Selected: " + _minuteIntervalList[_minuteInterval] + " min",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                      Text(
+                        _lowerBound[_minuteInterval].toString() + "% - " + _upperBound[_minuteInterval].toString() + "%",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                    ],
+                  ) : Container(),
+                ],
+              ),
             ),
-            Consumer<OxygenSaturationProvider>(
-                builder: (context, provider, widget) {
-                  return Slider(
-                    value: _currSat,
-                    min: 50,
-                    max: 100,
-                    divisions: 10,
-                    label: _currSat.toString() + "%",
-                    onChanged: (double value) => provider.setSaturationPercentage(value.toInt()),
-                  );
-                }
+            SizedBox(height: 40),
+            Slider(
+              value: _saturationPercentage.toDouble(),
+              min: 50,
+              max: 100,
+              divisions: 50,
+              onChanged: (double value) => {
+                setState((){
+                  _saturationPercentage = value.toInt();
+                  updateStatus(_minuteMSList[_minuteInterval]);
+                })
+              },
             ),
-            
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: OxygenSaturationTable(),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class OxygenSaturationTable extends StatelessWidget {
+
+  const OxygenSaturationTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      border: TableBorder(
+        top: const BorderSide(color: Color(0xffeaeaea), width: 1),
+        right: const BorderSide(color: Color(0xffeaeaea), width: 1),
+        left: const BorderSide(color: Color(0xffeaeaea), width: 1),
+        bottom: const BorderSide(color: Color(0xffeaeaea), width: 1),
+      ),
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      columnWidths: const <int, TableColumnWidth>{
+        0: FixedColumnWidth(150),
+        1: FlexColumnWidth(),
+      },
+      children: [
+        TableRow(
+            children: [
+              TableCell(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xff9F97E3),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "ELAPSED TIME",
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              TableCell(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xff9F97E3),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "TARGET OXYGEN SATURATION",
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ]
+        ),
+        TableRow(
+            children: [
+              TableCell(
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("1 min", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+                ),
+              ),
+              TableCell(
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("60% - 65%", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300)),
+                ),
+              ),
+            ]
+        ),
+        TableRow(
+          children: [
+            TableCell(
+              child: Container(
+                color: Colors.grey[50],
+                padding: EdgeInsets.all(8),
+                alignment: Alignment.center,
+                child: Text("2 min", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+              ),
+            ),
+            TableCell(
+              child: Container(
+                color: Colors.grey[50],
+                padding: EdgeInsets.all(8),
+                alignment: Alignment.center,
+                child: Text("65% - 70%", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300)),
+              ),
+            ),
+          ],
+        ),
+        TableRow(
+            children: [
+              TableCell(
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("3 min", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+                ),
+              ),
+              TableCell(
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("70% - 75%", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300)),
+                ),
+              ),
+            ]
+        ),
+        TableRow(
+            children: [
+              TableCell(
+                child: Container(
+                  color: Colors.grey[50],
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("4 min", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+                ),
+              ),
+              TableCell(
+                child: Container(
+                  color: Colors.grey[50],
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("75% - 80%", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300)),
+                ),
+              ),
+            ]
+        ),
+        TableRow(
+            children: [
+              TableCell(
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("5 min", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+                ),
+              ),
+              TableCell(
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("80% - 85%", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300)),
+                ),
+              ),
+            ]
+        ),
+        TableRow(
+            children: [
+              TableCell(
+                child: Container(
+                  color: Colors.grey[50],
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("10 min", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+                ),
+              ),
+              TableCell(
+                child: Container(
+                  color: Colors.grey[50],
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text("85% - 95%", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300)),
+                ),
+              ),
+            ]
+        ),
+      ],
     );
   }
 }
